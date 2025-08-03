8c4f2f0b0ed010f12b1f7b4040711b5618c76199
Ahmed Naviwala <154126408+ahmedST7204@users.noreply.github.com>
2025-08-03 20:53:45 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,21 +1,25 @@
-const display = document.forms['calcForm']['display'];
+let display = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let buttonArray = Array.from(buttons);
+let string = '';
 
-        function appendValue(value) {
-            display.value += value;
-        }
+buttonArray.forEach(btn => {
+    btn.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            display.value = string;
 
-        function clearDisplay() {
-            display.value = '';
-        }
+        }else if(e.target.innerHTML == 'AC'){
+            string = '';
+            display.value = string;
 
-        function deleteLast() {
-            display.value = display.value.toString().slice(0, -1);
+        }else if(e.target.innerHTML == '='){
+            string = eval(string);
+            display.value = string;
+            
+        }else{
+            string += e.target.innerHTML;
+            display.value = string;
         }
-
-        function calculate() {
-            try {
-                display.value = eval(display.value);
-            } catch {
-                display.value = 'Error';
-            }
-        }
+    });
+});
