63e647ca72f2974273a3c853875589ce0909e715
Ahmed Naviwala <154126408+ahmedST7204@users.noreply.github.com>
2025-08-05 12:19:06 +0530

    fixed some code

--- Unit_Converter/script.js
+++ Unit_Converter/script.js
@@ -1,89 +1,95 @@
 var meter = document.getElementById('meter');
 var centimeter = document.getElementById('centimeter');
-var inches = document.getElementById('inches')
-var centimeters = document.getElementById('centimeters')
+var inches = document.getElementById('inches');
+var centimeters = document.getElementById('centimeters');
 var feet = document.getElementById('feet');
 var inch = document.getElementById('inch');
 var kilometers = document.getElementById('kilometers');
 var meters = document.getElementById('meters');
-var liter = document.getElementById('liter')
-var mililiter = document.getElementById('mililiter')
+var liter = document.getElementById('liter');
+var mililiter = document.getElementById('mililiter');
 
-// -------------------Meter--------------------
+// --------------------- Meter ------------------------
 meter.addEventListener('input', function(){
     let m = this.value;
     let cm = m*100;
     centimeter.value = cm;
 });
 
-// -----------------Centimeter--------------------
+// -------------------- Centimeter ----------------------
 centimeter.addEventListener('input', function(){
-    let c = this.value;
-    let m = c/100;
+    let cm = this.value;
+    let m = cm/100;
+        if(!Number.isInteger(m)){
+        m = m.toFixed(2);
+    }
     meter.value = m;
 });
 
-// ----------------Inches----------------------
+// ------------------- inches --------------------
 inches.addEventListener('input', function(){
     let ins = this.value;
     let cms = ins*2.54;
-        if(!Number.isInteger(cms)){
-            cms = cms.toFixed(2);
-    }
     centimeters.value = cms;
 });
 
-// ----------------Centimeters-------------------
+// -------------------- Centimeters ----------------------
 centimeters.addEventListener('input', function(){
     let cms = this.value;
     let ins = cms/2.54;
-     if(!Number.isInteger(ins)){
+        if(!Number.isInteger(ins)){
         ins = ins.toFixed(2);
     }
     inches.value = ins;
 });
 
-// ------------------Feet----------------------
+// -------------------- Centimeter ----------------------
 feet.addEventListener('input', function(){
     let f = this.value;
     let i = f*12;
     inch.value = i;
 });
 
-// -------------------Inch----------------------
+// ----------------------- Inch -------------------------
 inch.addEventListener('input', function(){
     let i = this.value;
     let f = i/12;
-    if(!Number.isInteger(f)){
+        if(!Number.isInteger(f)){
         f = f.toFixed(2);
     }
     feet.value = f;
 });
 
-// -----------------KiloMeters-------------------
+// -------------------- Kilometers ----------------------
 kilometers.addEventListener('input', function(){
-    let km = this.value;
-    let ms = km*1000;
+    let kms = this.value;
+    let ms = kms*1000;
     meters.value = ms;
 });
 
-// -------------------Meters----------------------
+// ----------------------- Meter ----------------------------
 meters.addEventListener('input', function(){
     let ms = this.value;
-    let km = ms/1000;
-    kilometers.value = km;
+    let kms = ms/1000;
+        if(!Number.isInteger(kms)){
+        kms = kms.toFixed(2);
+    }
+    kilometers.value = kms;
 });
 
-// ---------------------Liter-----------------------
+// -------------------------- Liter ------------------------------
 liter.addEventListener('input', function(){
     let l = this.value;
     let ml = l*1000;
     mililiter.value = ml;
 });
 
-// -------------------Mililiter----------------------
+// ------------------------ Mililiter ---------------------------
 mililiter.addEventListener('input', function(){
     let ml = this.value;
     let l = ml/1000;
+    if(!Number.isInteger(l)){
+        l = l.toFixed(2);
+    }
     liter.value = l;
 });
